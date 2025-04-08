6cad60f023408a35e2bc98184813ee4a8310ff3a
kotteletfisk <ostekugle@gmail.com>
2025-04-08 13:44:59 +0200

    clustering

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
@@ -9,7 +10,7 @@ from sklearn.decomposition import PCA
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, DBSCAN
 from scipy.stats import zscore
 import scipy.cluster.hierarchy as ch
 from sklearn.cluster import AgglomerativeClustering
@@ -113,6 +114,72 @@ if page == "Random forest regression":
     st.pyplot(plt)
 
 
-if page == "PCA and clustering":
+if page == "Clustering":
     
-    st.write("PCA and clustering")
+    st.header("Clustering")
+    st.write("The k-means algorithm needs to know the number of clusters to find in the dataset." \
+    "try and find the amount of cluster that makes the most sense!")
+
+    df_pca = pd.read_pickle("../data/coffee_pca.pkl")
+    n_clusters = st.number_input(min_value=1, max_value=10, label="Number clusters")
+
+    if (st.button("Predict k-means") and n_clusters):
+        kmeans = KMeans(init='k-means++', n_clusters=n_clusters, n_init=10) 
+        prediction = kmeans.fit_predict(df_pca)
+
+        plt.scatter(df_pca['PC_1'], df_pca['PC_2'], c=prediction, s=50, cmap='viridis')
+        plt.xlabel('PC_1')
+        plt.ylabel('PC_2')
+        plt.grid(True)
+        st.pyplot(plt)
+    
+    # Dendogram for agglomerative clustering
+    st.header("Agglomerative Clustering")
+    st.write("For finding out the optimal number of cluster by euclidean distance of data points. One can use a dendogram to visualize it potential cluster amount.")
+    dendo_png = mpimg.imread("../data/dendogram.png")
+    st.image(dendo_png)
+
+    n_clusters_agg = st.number_input(min_value=1, max_value=10, label="Number clusters", key="2")
+    if (st.button("Predict Agglomerative clustering") and n_clusters_agg):
+        model = AgglomerativeClustering(n_clusters_agg, linkage = 'ward')
+        aggmodel_pred = model.fit_predict(df_pca)
+
+        plt.scatter(df_pca['PC_1'], df_pca['PC_2'], c=aggmodel_pred, s=50, cmap='viridis')
+        plt.xlabel('PC_1')
+        plt.ylabel('PC_2')
+        plt.grid(True)
+        st.pyplot(plt)
+
+    st.header("DBSCAN")
+    st.write("Dbscan can automatically determine how many clusters are present using a different set of parameters. ")
+    st.markdown("#### **eps**")
+    st.write("""
+            The maximum distance between two samples for one to be considered as in the neighborhood of the other.
+            This is not a maximum bound on the distances of points within a cluster.
+            This is the most important DBSCAN parameter to choose appropriately for your data set and distance function.
+            """)
+    st.markdown("#### **min_samples**")
+    st.write("""
+            The number of samples (or total weight) in a neighborhood for a point to be considered as a core point.
+            This includes the point itself. If min_samples is set to a higher value, DBSCAN will find denser clusters, 
+            whereas if it is set to a lower value, the found clusters will be more sparse.
+            """)
+
+    eps = st.number_input(min_value=0.1, max_value=10.0, value=0.40, label="eps", key="3")
+    min_s = st.number_input(min_value=1, max_value=10, value=6, label="Min samples", key="4")
+
+    if (st.button("Predict DBSCAN") and eps and min_s):
+        dbscan = DBSCAN(eps=eps, min_samples=min_s)
+        dbscan_pred = dbscan.fit_predict(df_pca)
+
+        plt.scatter(df_pca['PC_1'], df_pca['PC_2'], c=dbscan_pred, s=50, cmap='viridis')
+        plt.xlabel('PC_1')
+        plt.ylabel('PC_2')
+        plt.grid(True)
+        plt.legend()
+        st.pyplot(plt)
+
+
+
+
+
